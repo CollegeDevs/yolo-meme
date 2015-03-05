2a090f2d52bd39cca1e4caa2a8d826aca1015f13
therise3107 <lyadav3107@gmail.com>
2015-03-05 21:21:58 +0530

    loaded emoji

--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.paths << Emoji.images_path
-config.assets.precompile << "emoji/**/*.png"
 
 module Work3
   class Application < Rails::Application
@@ -21,5 +19,8 @@ module Work3
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.paths << Emoji.images_path
+    config.assets.precompile << "emoji/**/*.png"
+
   end
 end
